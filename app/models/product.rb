6892c70f5c42bdbe3dfc5a5a232870d3bd93cebb
Thomas Petrachi <thomas.petrachi@vodeclic.com>
2014-03-01 19:07:53 +0100

    add validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  scope :published, where(published: true)
+
+  validates :user, :category, :name, :brief, :descr, presence: true
+  validates :price, presence: true, :numericality => {:greater_than => 4}
+
+  before_create def set_defaults
+    self.stock ||= 1
+    self.public = true if self.public.nil?
+    self.published = true if self.published.nil?
+  end
 end
