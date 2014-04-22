337006b63b7fd2d5c40c94956a71b8a2e5179a73
Adrien Folie <folie.adrien@gmail.com>
2014-04-22 10:51:35 +0200

    Create user.rb

---
+++ app/controllers/concerns/user.rb
@@ -0,0 +1,17 @@
+module User
+  def self.included(base)
+    base.class_eval do
+       layout "users"
+
+       before_action :set_user
+    end
+  end
+  
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+  
+  def has_ownership?
+    current_user && @user.id == current_user.id
+  end
+end
